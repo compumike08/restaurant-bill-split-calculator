e09ef98be95c83e374c1f7d1b7fe08afbabdddf0
compumike08 <compumike08@users.noreply.github.com>
2015-11-21 22:14:18 -0500

    Add unit test for appConfig function

---
+++ test/app/app.spec.js
@@ -0,0 +1,17 @@
+describe('myApp', function () {
+    var $urlRouterProvider;
+    
+    beforeEach(function () {
+        module(function(_$urlRouterProvider_) {
+            $urlRouterProvider = _$urlRouterProvider_;
+            spyOn($urlRouterProvider, 'otherwise');
+        });
+        module('myApp');
+        inject();
+    });
+    
+    it('should set $urlRouterProvider.otherwise to true', function() {
+        expect($urlRouterProvider.otherwise)
+            .toHaveBeenCalledWith(true);
+    });
+});
