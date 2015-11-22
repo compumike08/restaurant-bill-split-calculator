ee5906c5df65f5a4e397e1c1b590552675d1bc62
compumike08 <compumike08@users.noreply.github.com>
2015-11-21 23:56:42 -0500

    Fix syntax in home.spec.js

--- src/app/home/home.spec.js
+++ src/app/home/home.spec.js
@@ -1,16 +1,21 @@
 'use strict';
 
 describe('myApp.home module', function() {
+    
+    var homeCtrl;
 
   beforeEach(module('myApp.home'));
 
   describe('home controller', function(){
-
-    it('should ....', inject(function($controller) {
-      //spec body
-      var homeCtrl = $controller('HomeCtrl');
-      expect(homeCtrl).toBeDefined();
+      
+    beforeEach(inject(function($controller){
+        homeCtrl = $controller('HomeCtrl');
     }));
+
+    it('should ....', function(){
+      //spec body
+      expect(homeCtrl).toBeDefined();
+    });
 
   });
 });
